cdf1bfa53b0bb47afe6b4dc6140a0a6e043c6fc3
Priyanka <priyankagangane2001@gmail.com>
2024-02-28 11:55:32 +0530

    Added @babel/plugin-proposal-private-property-in-object as a dev dependency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import RegisterPage from "./Components/Authentication/RegisterPage";
 import LoginPage from "./Components/Authentication/LoginPage";
