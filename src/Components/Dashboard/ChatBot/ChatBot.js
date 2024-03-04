91e8a15e147f79e728b6854802d4e1468b8e4a7f
PriyankaGangane <91834648+PriyankaGangane@users.noreply.github.com>
2024-03-04 13:57:40 +0530

    save

--- src/Components/Dashboard/ChatBot/ChatBot.js
+++ src/Components/Dashboard/ChatBot/ChatBot.js
@@ -4,11 +4,14 @@ import React from 'react';
 class ChatbotComponent extends React.Component {
   render() {
     return (
-      <div className="chatbot-container">
-        
-          <iframe src="https://webchat.botframework.com/embed/mylangbotazure-bot?s=77Ydj8MfPnI.hVwx7fRXLEORdBmf0LvuHUA_s_xRbTWJ8q732pdW41Y" style="height: 502px; max-height: 502px;"></iframe>
-        
-      </div>
+
+        <div className="chatbot-container">
+    <iframe title="Chat Bot"
+    src="https://webchat.botframework.com/embed/mylangbotazure-bot?s=77Ydj8MfPnI.hVwx7fRXLEORdBmf0LvuHUA_s_xRbTWJ8q732pdW41Y"
+    style={{ position: "absolute", right: 0, bottom: "3%", height: "502px", maxHeight: "502px" }}
+  ></iframe>
+    </div>
+
     );
   }
 }
