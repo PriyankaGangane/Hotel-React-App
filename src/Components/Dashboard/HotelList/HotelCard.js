41b7f1a049c95449ef13ad247bc63541671268ec
PriyankaGangane <91834648+PriyankaGangane@users.noreply.github.com>
2024-03-04 14:31:58 +0530

    save chage

--- src/Components/Dashboard/HotelList/HotelCard.js
+++ src/Components/Dashboard/HotelList/HotelCard.js
@@ -3,7 +3,7 @@ import "./HotelCard.css"; // Custom CSS for styling the hotel card
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import axios from "axios"; // Import axios for making HTTP requests
 
-const HotelCard = ({ image, name, address, rating, id, user, onDelete }) => {
+const HotelCard = ({ image, name, address, price, id, user, onDelete }) => {
   console.log(user, "user");
 
   return (
@@ -11,7 +11,7 @@ const HotelCard = ({ image, name, address, rating, id, user, onDelete }) => {
       <img src={image} alt={name} className="hotel-img" />
       <div className="hotel-details">
         <p className="hotel-description">{address}</p>
-        <p className="hotel-price">Price: ${rating} per night</p>
+        <p className="hotel-price">Price: ${price} per night</p>
         {user.email ? (
           <div className="button-container">
             <button className="edit-button">
